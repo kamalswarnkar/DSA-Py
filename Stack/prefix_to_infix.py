158d33e858e0583751201d80ae7841efd103727a
Kamal Swarnkar <swarnkar889@gmail.com>
2026-05-12 12:49:15 +0530

    Prefix to Infix Conversion!

--- Stack/prefix_to_infix.py
+++ Stack/prefix_to_infix.py
@@ -3,15 +3,19 @@ def pre_to_in(exp):
 
     ops = set("+-*/%^")
 
-    for ch in reversed(exp):
+    for ch in reversed(exp): #prefix traversal: right -> left
         if ch not in ops:
             stack.append(ch)
         else:
             op1 = stack.pop()
             op2 = stack.pop()
+            """
+            here we will pop left operand first, 
+            and then right operand
+            """
 
             tmp = "(" + op1 + ch + op2 + ")"
 
             stack.append(tmp)
     
-    return stack[-1]
+    return stack[-1] # stack is LIFO based, so the final expression remains at the top of stack
